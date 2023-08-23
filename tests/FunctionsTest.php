cff6c97384067750f3d294bb6666234d43c491a0
Sam Millar <smillar36@qub.ac.uk>
2023-08-23 16:24:36 +0100

    Percents CI #17 (sem-push.sh)

--- tests/FunctionsTest.php
+++ tests/FunctionsTest.php
@@ -5,9 +5,8 @@ use PHPUnit\Framework\TestCase;
 
 class FunctionsTest extends TestCase {
 
-    public array $extractDataSuites = array(
-        /// Some translated from js to PHP by GPT-3.5
-        "valid" => array(
+    public function provideExtractDataValid() {
+        return [
             array(
                 array(
                     'item_1' => "Lecture sessions",
@@ -66,9 +65,11 @@ class FunctionsTest extends TestCase {
                     'availabilities' => array(60, 60),
                 )
             )
+                ];
+    }
 
-        ),
-        'diffCounts' => array(
+    public function provideExtractDataDiffCounts() {
+        return [
             array(
                 array(
                     'item_1' => "Lectures",
@@ -123,24 +124,151 @@ class FunctionsTest extends TestCase {
                     'attendance_4' => 55
                 )
             )
-        )      
+        ];
+    }
 
-    );
+    // public array $extractDataSuites = array(
+    //     /// Some translated from js to PHP by GPT-3.5
+    //     "valid" => array(
+    //         array(
+    //             array(
+    //                 'item_1' => "Lecture sessions",
+    //                 'item_2' => "Lab sessions",
+    //                 'item_3' => "Support sessions",
+    //                 'item_4' => "Canvas activities",
+    //                 'attendance_1' => 0,
+    //                 'attendance_2' => 1,
+    //                 'attendance_3' => 10,
+    //                 'attendance_4' => 55,
+    //                 'availability_1' => 33,
+    //                 'availability_2' => 22,
+    //                 'availability_3' => 44,
+    //                 'availability_4' => 55,
+    //             ),
+    //             array(
+    //                 "items" => array("Lecture sessions","Lab sessions","Support sessions","Canvas activities"),
+    //                 "attendances" => array(0,1,10,55),
+    //                 "availabilities" => array(33,22,44,55)   
+    //             )
+    //         ),
+    //         array(
+    //             array(
+    //                 'foo' => "bar",
+    //                 'item_1' => "Lectures",
+    //                 'item_2' => "Labs",
+    //                 'item_3' => "Support",
+    //                 'item_4' => "Canvas",                
+    //                 'attendance_1' => 4,
+    //                 'attendance_2' => 22.5,
+    //                 'attendance_3' => 0,
+    //                 'attendance_4' => 50,
+    //                 'availability_1' => 60,
+    //                 'availability_2' => 60,
+    //                 'availability_3' => 60,
+    //                 'availability_4' => 60,
+    //             ),
+    //             array(
+    //                 "items" => array("Lectures","Labs","Support","Canvas"),
+    //                 'attendances' => array(4, 22.5, 0, 50),
+    //                 'availabilities' => array(60, 60, 60, 60),
+    //             )
+    //         ),
+    //         array(
+    //             array(
+    //                 'item_1' => "Lectures",
+    //                 'item_2' => "Labs",
+    //                 'attendance_1' => 4,
+    //                 'attendance_2' => 22,
+    //                 'availability_1' => 60,
+    //                 'availability_2' => 60,
+    //             ),
+    //             array(
+    //                 "items" => array("Lectures","Labs"),
+    //                 'attendances' => array(4, 22),
+    //                 'availabilities' => array(60, 60),
+    //             )
+    //         )
 
-    public function testExtractData() {
+    //     ),
+    //     'diffCounts' => array(
+    //         array(
+    //             array(
+    //                 'item_1' => "Lectures",
+    //                 'item_2' => "Labs",
+    //                 'item_3' => "Support",
+    //                 'item_4' => "Canvas",                
+    //                 'attendance_1' => 0,
+    //                 'attendance_2' => 1,
+    //                 'availability_1' => 33,
+    //                 'availability_2' => 22,
+    //                 'availability_3' => 44,
+    //                 'availability_4' => 9
+    //             )
+    //         ),
+    //         array(
+    //             array(
+    //                 'item_1' => "Lectures",
+    //                 'item_2' => "Labs",
+    //                 'item_3' => "Support",
+    //                 'item_4' => "Canvas",                
+    //                 'attendance_1' => 0,
+    //                 'attendance_2' => 1,
+    //                 'attendance_3' => 10,
+    //                 'attendance_4' => 55,
+    //                 'availability_1' => 33,
+    //                 'availability_2' => 22,
+    //                 'availability_3' => 44
+    //             )
+    //         ),
+    //         array(
+    //             array(
+    //                 'item_1' => "Lectures",            
+    //                 'attendance_1' => 0,
+    //                 'attendance_2' => 1,
+    //                 'attendance_3' => 10,
+    //                 'attendance_4' => 55,
+    //                 'availability_1' => 33,
+    //                 'availability_2' => 22,
+    //                 'availability_3' => 44,
+    //                 'availability_4' => 55
+    //             )
+    //         ),
+    //         array(
+    //             array(
+    //                 'item_1' => "Lectures",
+    //                 'item_2' => "Labs",
+    //                 'item_3' => "Support",
+    //                 'item_4' => "Canvas",                
+    //                 'attendance_1' => 0,
+    //                 'attendance_2' => 1,
+    //                 'attendance_3' => 10,
+    //                 'attendance_4' => 55
+    //             )
+    //         )
+    //     )      
 
-        echo $this->$extractDataSuites;
+    // );
+
+    /**
+     * @dataProvider provideExtractDataValid
+     */
+    public function testExtractDataValid() {
 
         $functions = new Functions();
-        $casesValid = (array)($this->$extractDataSuites['valid']);
-        // echo count($casesValid);
-        $casesDiffCounts = (array)($this->$extractDataSuites['diffCounts']);
-        // echo count($casesDiffCounts);
 
         foreach ($casesValid as $case) {
             $actual = $functions->extractData($case[0]);
             $this->assertEqualsCanonicalizing($case[1], $actual);
         }
+
+    }
+
+    /**
+     * @dataProvider provideExtractDataDiffCounts
+     */
+    public function testExtractDataDiffCounts() {
+
+        $functions = new Functions();
 
         foreach ($casesDiffCounts as $case) {
             $this->expectException(Exception::class);
